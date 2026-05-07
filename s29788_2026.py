a92b5d3d11c1b82c4abdf4069719bececbdd4570
s29788 <s29788@pjwstk.edu.pl>
2026-05-08 01:48:08 +0200

    pozostałe funkcjonalności

--- s29788_2026.py
+++ s29788_2026.py
@@ -7,6 +7,23 @@ import random, math, string
 
 nucleotides = ["A", "C", "G", "T"]
 
+codons = {"TTT":"F", "TCT":"S", "TAT":"Y", "TGT":"C",
+          "TTC":"F", "TCC":"S", "TAC":"Y", "TGC":"C",
+          "TTA":"L", "TCA":"S", "TAA":"*", "TGA":"*",
+          "TTG":"L", "TCG":"S", "TAG":"*", "TGG":"W",
+          "CTT":"L", "CCT":"P", "CAT":"H", "CGT":"R",
+          "CTC":"L", "CCC":"P", "CAC":"H", "CGC":"R",
+          "CTA":"L", "CCA":"P", "CAA":"Q", "CGA":"R",
+          "CTG":"L", "CCG":"P", "CAG":"Q", "CGG":"R",
+          "ATT":"I", "ACT":"T","AAT":"N","AGT":"S",
+          "ATC":"I", "ACC":"T", "AAC":"N", "AGC":"S",
+          "ATA":"I", "ACA":"T", "AAA":"K", "AGA":"R",
+          "ATG":"M", "ACG":"T", "AAG":"K", "AGG":"R",
+          "GTT":"V", "GCT":"A", "GAT":"D", "GGT":"G",
+          "GTC":"V", "GCC":"A", "GAC":"D", "GGC":"G",
+          "GTA":"V", "GCA":"A", "GAA":"E", "GGA":"G",
+          "GTG":"V", "GCG":"A", "GAG":"E", "GGG":"G"}
+
 def generate_sequence(length: int) -> str:
     """
     Generuje losową sekwencję nukleotydową o podanej długości.
@@ -71,7 +88,7 @@ def insert_name(sequence: str, name: str) -> str:
     Returns:
         str: sekwencja z wstawionym imieniem
     """
-    name.lower()
+    name = name.lower()
 
     if len(sequence) == 0:
         return name
@@ -204,7 +221,75 @@ def remove_name(sequence: str) -> str:
     table = str.maketrans("", "", string.ascii_lowercase)
     return sequence.translate(table)
 
-def generate_compliment_sequence(sequence: str) -> str:
+def validate_nucleotide_distribution(a: int, t: int, c: int, g: int) -> int:
+    """
+    Sprawdza, czy suma procentów nukleotydów wynosi 100.
+
+    Args:
+        a (int): procent A
+        t (int): procent T
+        c (int): procent C
+        g (int): procent G
+
+    Returns:
+        int:
+            1 - suma wynosi 100
+            0 - suma nie wynosi 100
+    """
+    if a + t + c + g == 100:
+        return 1
+    else:
+        return 0
+
+def generate_sequence_with_nucleotide_distribution(a_proc: int, t_proc: int, c_proc: int, g_proc: int, length: int) -> str:
+    """
+    Generuje losową sekwencję DNA z podanym procentowym udziałem nukleotydów.
+    Oblicza ilość wystąpień każdego z nukleotydów w sekwencji o podanej długości.
+    Losuje pozycję nukleotydu z listy nukleotydów, jeśli nie przekroczono liczby wystąpień wylosowanego nukleotydu w sekwencji, metoda dodaje go do sekwencji wynikowej.
+
+    Args:
+        a_proc (int): procent A
+        t_proc (int): procent T
+        c_proc (int): procent C
+        g_proc (int): procent G
+        length (int): długość sekwencji
+
+    Returns:
+        str: wygenerowana sekwencja DNA z uwzględnionym procentowym udziałem każdego z nukleotydów
+    """
+    a_count = round(a_proc * length / 100)
+    t_count = round(t_proc * length / 100)
+    c_count = round(c_proc * length / 100)
+    g_count = length - a_count - t_count - c_count
+
+    seq = ""
+    i = length
+    while i > 0:
+        rand = random.randint(0, len(nucleotides)-1)
+        if nucleotides[rand] == "A":
+            if a_count > 0:
+                seq += nucleotides[rand]
+                a_count -= 1
+                i -= 1
+        elif nucleotides[rand] == "T":
+            if t_count > 0:
+                seq += nucleotides[rand]
+                t_count -= 1
+                i -= 1
+        elif nucleotides[rand] == "C":
+            if c_count > 0:
+                seq += nucleotides[rand]
+                c_count -= 1
+                i -= 1
+        elif nucleotides[rand] == "G":
+            if g_count > 0:
+                seq += nucleotides[rand]
+                g_count -= 1
+                i -= 1
+
+    return seq
+
+def generate_complement_sequence(sequence: str) -> str:
     """
     Generuje sekwencję komplementarną DNA.
     Tworzy tabelę mapującą przy pomocy metody maketrans({"A": "T", "T": "A", "C": "G", "G": "C"}), która po użyciu metody translate() zamienia w sekwencji, na której jest wywołana A<->T, G<->C.
@@ -222,7 +307,7 @@ def generate_compliment_sequence(sequence: str) -> str:
 
     return sequence
 
-def generate_reverse_compliment_sequence(sequence: str) -> str:
+def generate_reverse_complement_sequence(sequence: str) -> str:
     """
     Generuje odwrotnie komplementarną sekwencję DNA.
     Usuwa imię z podanej sekwencji za pomocą metody remove_name().
@@ -236,7 +321,7 @@ def generate_reverse_compliment_sequence(sequence: str) -> str:
         str: odwrotnie komplementarna sekwencja DNA
     """
     sequence = remove_name(sequence)
-    sequence = generate_compliment_sequence(sequence)
+    sequence = generate_complement_sequence(sequence)
     sequence = sequence[::-1]
     return sequence
 
@@ -255,19 +340,49 @@ def transcription(sequence: str) -> str:
     """
     if len(sequence) == 0:
         return sequence
+    sequence = remove_name(sequence)
+    return sequence.replace("T", "U")
 
-    seq = remove_name(sequence)
-    return seq.replace("T", "U")
+def translation(sequence: str) -> str:
+    """
+    Przeprowadza translację sekwencji DNA.
+    Zwraca pusty string, jeśli sekwencja DNA jest pusta.
+    Usuwa imię z podanej sekwencji za pomocą metody remove_name().
+    Iteruje po podanej sekwencji DNA co trzy nukleotydy,łącząc każdą trójkę nukleotydów w jeden kodon.
+    Sprawdza, czy powstały kodon znajduje się w tablicy kodonów, jeśli kodona nie ma w tablicy, przechodzi do kolejnej iteracji, jeśli kodon jest kodonem stop, kończy iteracje.
+    Dodaje do wynikowej sekwencji aminokwasów aminokwas, któremu odpowiada znaleziony kodon w każdej iteracji.
+
+    Args:
+        sequence (str): sekwencja DNA
+
+    Returns:
+        str: sekwencja aminokwasowa
+    """
+    if len(sequence) == 0:
+        return sequence
+    sequence = remove_name(sequence)
+
+    seq = ""
+    for i in range(0, len(sequence)-2, 3):
+        codon = sequence[i:i+3]
+        if codon not in codons:
+            continue
+        if codons[codon] == "*":
+            break
+        seq += codons[codon]
+    return seq
+
 
 def main():
     """
     Główna funkcja programu odpowiedzialna za:
     - pobieranie danych od użytkownika,
-    - generowanie sekwencji,
+    - generowanie sekwencji z uwzględnieniem procentowego udziału każdego z nukleotydów,
     - zapis do pliku FASTA,
     - wyświetlanie statystyk,
     - generowanie i zapisywanie do pliku sekwencji komplementarnej i odwrotnie komplementarnej,
-    - transkrypcję sekwencji i zapisywanie wyniku do pliku
+    - transkrypcję sekwencji i zapisanie wyniku do pliku
+    - translację sekwencji i zapisanie wyniku do pliku
     """
 
     var = 0
@@ -284,7 +399,17 @@ def main():
 
     seq_desc = str(input("podaj opis sekwencji "))
 
-    seq = generate_sequence(int(seq_length))
+    var = 0
+    while var != 1:
+        a = int(input("podaj w % udział nukleotydu A w sekwencji "))
+        t = int(input("podaj w % udział nukleotydu T w sekwencji "))
+        c = int(input("podaj w % udział nukleotydu C w sekwencji "))
+        g = int(input("podaj w % udział nukleotydu G w sekwencji "))
+        var = validate_nucleotide_distribution(a,t,c,g)
+        if var == 0:
+            print("łączny procentowy udział nukleotydów w sekwencji ma wynosić 100%")
+
+    seq = generate_sequence_with_nucleotide_distribution(a,t,c,g,int(seq_length))
 
     name = str(input("podaj imie "))
     seq = insert_name(seq, name)
@@ -297,11 +422,14 @@ def main():
     print(calculate_stats(seq))
 
     with open(file_name, mode="a", encoding="utf-8") as file:
-        file.write("sekwencja komplementarna\n" + format_sequence(generate_compliment_sequence(seq), 80)+"\n")
-        file.write("sekwencja odwrotnie komplementarna\n" + format_sequence(generate_reverse_compliment_sequence(seq), 80)+"\n")
+        file.write("sekwencja komplementarna\n" + format_sequence(generate_complement_sequence(seq), 80) + "\n")
+        file.write("sekwencja odwrotnie komplementarna\n" + format_sequence(generate_reverse_complement_sequence(seq), 80) + "\n")
 
     with open(file_name, mode="a", encoding="utf-8") as file:
         file.write("po transkrypcji\n" + format_sequence(transcription(seq), 80) + "\n")
+
+    with open(file_name, mode="a", encoding="utf-8") as file:
+        file.write("po translacji\n" + format_sequence(translation(seq), 80)+"\n")
 
 if __name__ == "__main__":
     main()
